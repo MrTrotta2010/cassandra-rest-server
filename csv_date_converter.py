62c619f09a95541b3e0dd9fce508d908edad653a
diegocolombo <diegodias@ufsj.edu.br>
2021-04-14 14:06:43 +0000

    ReBase Date Converter code

--- csv_date_converter.py
+++ csv_date_converter.py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[51]:
-
-
 # importing the pandas library
 import pandas as pd
-  
+
 # reading the csv file
 df = pd.read_csv("backup.csv")
-  
-# updating the column value/data
-#df['insertiondate'] = df['insertiondate'].replace({'2021-04-14T18:00:01.000Z'})
 
 for i in range(df.shape[0]):
     #print(df['insertiondate'][i])
@@ -23,6 +14,6 @@ for i in range(df.shape[0]):
 
 # writing into the file
 df.to_csv("backupnovo.csv", index=False)
-  
+
 print(df)
 
